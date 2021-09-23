e928cfe6b1d3903df88843749995e800c1624c16
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-23 16:29:07 +0900

    Create phone_number.java

---
+++ programmers/src/programmers/phone_number.java
@@ -0,0 +1,15 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class phone_number {
+
+	public static void main(String[] args) {
+		//subString 사용하기
+		//s는 4자리 이상 20 자리 미만
+		System.out.println("핸드폰 번호를 숫자만 입력해주세요");
+		Scanner sc = new Scanner(System.in);
+		String num = sc.nextLine();
+	}
+
+}
