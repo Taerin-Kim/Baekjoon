ed87be76efc7fbfde337238d2d5c372b6d4e2244
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-25 18:07:22 +0900

    test

---
+++ programmers/src/programmers/Even_Odd2.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Even_Odd2 {
+
+	public static void main(String[] args) {
+		System.out.println("짝수와 홀수, 정수만 입력해주세요");
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+
+		String a = "Even";
+		String b = "Odd";
+		
+		if(num % 2 == 0) {
+			
+			System.out.printf("\"%s\"", a);
+			
+		} else {
+			System.out.printf("\"%s\"", b);
+		}
+
+	}
+
+}
