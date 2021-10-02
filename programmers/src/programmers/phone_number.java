8566366cbe6b737c4df4fc31ce2e9c204de96fc9
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-10-02 20:57:39 +0900

    hide phone number
    substring 이용하는 방법인데 정확히 이해하지 못하고 있음

--- programmers/src/programmers/phone_number.java
+++ programmers/src/programmers/phone_number.java
@@ -10,23 +10,35 @@ public class phone_number {
 		System.out.println("핸드폰 번호를 숫자만 입력해주세요");
 		Scanner sc = new Scanner(System.in);
 		String num = sc.nextLine();
-
-	}
-
-	private String solution(String phone_number) {
-		String answer = "";
-
-		int n = phone_number.length() - 4;
-		for (int i = 0; i < n; i++) {
-			answer += "*";
-		}
 		
-		return answer;
+		private String solution(String phone_number) {
+			String answer = "";
 
-		// 끝에서4글자 전까지for 문을 돌려 “*”을 찍고 String 의 + 성질을 이용
-
-		// toCharArray를 사용한 경우도 있음
-
+			int n = phone_number.length();
+			for (int i = 0; i < n-4; i++) {
+				answer += "*";
+			}
+			answer += phone_number.substring(n-4, n);
+			
+			return answer;
 	}
+	}
+
+	// 끝에서4글자 전까지for 문을 돌려 “*”을 찍고 String 의 + 성질을 이용
+
+	// toCharArray를 사용한 경우도 있음
+
+//		public String solution(String phone_number) {
+//			 
+//	        char[] ch = phone_number.toCharArray(); // 출력 01033334444
+//	        
+//	        for(int i = 0; i < ch.length - 4; i++) {
+//	            ch[i] = '*';
+//	            // 출력 *1033334444, **033334444, ***33334444, 
+//	            // ****3334444, *****334444, ******34444, *******4444
+//	        }
+//	 
+//	        return String.valueOf(ch);
+//	    }
 
 }
