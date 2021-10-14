081b9d1259c65814cbe0efed7653e70017272709
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-10-15 00:09:58 +0900

    int타입/다른방법으로
    String에 split 사용하지 말고할것

---
+++ programmers/src/programmers/plus_digitNum2.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class plus_digitNum2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//int타입으로 풀기
+	
+	int solution(int n) {
+	    int answer = 0;
+	    return answer;
+	}
+
+}
